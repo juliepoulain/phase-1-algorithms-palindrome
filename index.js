9a8438f08d59a63450cfa72f1c29297065998527
Julie Poulain <juliempoulain@gmail.com>
2024-03-14 10:43:36 -0700

    create palindrome function

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  let wordIndexLeft = 0;
+  let wordIndexRight = word.length - 1;
+  while (wordIndexLeft <= wordIndexRight) {
+    if (word[wordIndexLeft] !== word[wordIndexRight]) {
+      return false;
+    }
+    wordIndexLeft++;
+    wordIndexRight--;
+  }
+  return true;
 }
 
+console.log(isPalindrome("abba"));
+
 /* 
-  Add your pseudocode here
+  1. split string into character array
+  2. verify first and last character match
+  3. create loop iteration to increase/decrease index 
+  4. use "while" loop - to end when left is greater than or equal to right
+  5. inside loop - use if to determine if matching, if dont match, return false.
+  6. return true when loop is done (because you never returned a false)
 */
 
 /*
-  Add written explanation of your solution here
+if input is a palindrome, expect function to return true.
+If not, expect to return false. 
 */
 
 // You can run `node index.js` to view these console logs
